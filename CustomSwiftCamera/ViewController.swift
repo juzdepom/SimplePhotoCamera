63b136e637edb931a9582cd91852f77477f1f957
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-08-03 07:44:07 -0400

    Got the camera to work! Resolved .plist issue

--- CustomSwiftCamera/ViewController.swift
+++ CustomSwiftCamera/ViewController.swift
@@ -7,8 +7,18 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var captureSession = AVCaptureSession()
+    var backCamera: AVCaptureDevice?
+    var frontCamera: AVCaptureDevice?
+    var currentCamera: AVCaptureDevice?
+    
+    var photoOutput: AVCapturePhotoOutput?
+    
+    var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,23 +32,53 @@ class ViewController: UIViewController {
     }
     
     func setupCaptureSession(){
-        
+        //this gives the photo full resolution
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
     }
     
     func setupDevice(){
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: AVMediaType.video, position: AVCaptureDevice.Position.unspecified)
+        let devices = deviceDiscoverySession.devices
         
+        for device in devices {
+            if device.position == AVCaptureDevice.Position.back {
+                //if on back camera
+                backCamera = device
+            } else if device.position == AVCaptureDevice.Position.front {
+                //if on front camera
+                frontCamera = device
+            }
+            //by default, start with back camera
+            currentCamera = backCamera
+        }
     }
     
     func setupInputOutput(){
-        
+        //this can throw an error, which is why we use do/catch.
+        do {
+            let captureDeviceInput = try AVCaptureDeviceInput(device: currentCamera!)
+            captureSession.addInput(captureDeviceInput)
+            //set to .jpeg format
+            photoOutput?.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])], completionHandler: nil)
+        } catch {
+            print(error)
+        }
     }
     
     func setupPreviewLayer(){
-        
+        cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        print(cameraPreviewLayer!)
+        cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        cameraPreviewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+        //full screen camera
+        cameraPreviewLayer?.frame = self.view.frame
+        //at: 0 to make sure the button is not hidden.
+        self.view.layer.insertSublayer(cameraPreviewLayer!, at: 0)
     }
     
     func startRunningCaptureSession(){
-        
+        //start capturing data
+        captureSession.startRunning()
     }
 
     @IBAction func cameraButton_TouchUpInside(_ sender: Any) {
